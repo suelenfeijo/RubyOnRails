e52426ad0b0e0aa8ed85326b6ebd955984433a15
suelenfeijo <suelenfeijobr@gmail.com>
2024-02-04 21:22:19 -0300

    add validação caracters máximo descrição

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
     #a presença do título deve ser verdadeiro para ser valido o campo
-    validates :titulo , :descricao, presence:true
+    validates :descricao, presence:true
+    validates :titulo, presence:true , length: { minimum: 5 }   
+    validates :descricao, length: { maximum: 15}
 end
